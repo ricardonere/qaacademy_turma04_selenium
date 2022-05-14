8237c79e82b79ae47c2e21a6b41730bef13ef631
qaacademy <qaacademy@gmail.com>
2022-05-14 15:02:51 -0300

    Exercicio 4 - Listas LI e SELECT-Option

--- src/test/java/ExerciciosSelenium.java
+++ src/test/java/ExerciciosSelenium.java
@@ -3,7 +3,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class ExerciciosSelenium {
 
@@ -11,7 +13,7 @@ public class ExerciciosSelenium {
 
 
     @Test
-    public void testeExercicosSelenium() {
+    public void testeExercicosSelenium() throws InterruptedException {
 
         //Exercicio 01
         driver.get("file:///Users/mac/Documents/automation.html");
@@ -28,8 +30,28 @@ public class ExerciciosSelenium {
 
         //Exercicio03
         driver.findElement(By.xpath("//input[@id='checkbox1']")).click();
-        Assert.assertTrue("Checkbox não selecionado",driver.findElement(By.xpath("//input[@id='checkbox1']")).isSelected());
-        Assert.assertTrue("Checkbox selecionado", driver.findElement(By.xpath("//label[contains(text(),'Movies')]")).isSelected());
+        Assert.assertTrue("Checkbox não selecionado", driver.findElement(By.xpath("//input[@id='checkbox1']")).isSelected());
+        Assert.assertFalse("Checkbox selecionado", driver.findElement(By.xpath("//label[contains(text(),'Movies')]")).isSelected());
+
+        //Exercicio 04
+
+        driver.findElement(By.xpath("//div[@id='msdd']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[contains(text(),'Portuguese')]")).click();
+
+
+        //Listas Option
+
+        //com Click, tem que navegar até o elemento option e extrair o xpath
+        driver.findElement(By.xpath("//option[@value='Java']")).click();
+        Thread.sleep(2000);
+
+        //Selecionar Lista Option/Select com Select Selenium
+        WebElement selectElement = driver.findElement(By.xpath("//select[@id='Skills']"));
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("Linux");
+
+
 
     }
 
