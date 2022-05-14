0eb44c1e483473c2f82784c87e9c9ca227bd30e9
qaacademy <qaacademy@gmail.com>
2022-05-14 10:58:59 -0300

    Exercicio 01 Selenium

---
+++ src/test/java/ExerciciosSelenium.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExerciciosSelenium {
+
+    WebDriver driver = new ChromeDriver();
+
+
+    @Test
+    public void testeExercicosSelenium() {
+        driver.get("file:///Users/mac/Documents/automation.html");
+        driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]"))
+                .sendKeys("Flavio");
+        driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[@class='form-group']/div[2]/input[1]"))
+                .sendKeys("Dias");
+        driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("AV Paulista, 509");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("teste@teste.com.br");
+        driver.findElement(By.xpath(" //input[@type='tel']")).sendKeys("119773512");
+    }
+}
