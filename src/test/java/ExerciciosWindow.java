9fce419be4cab79e25a0090e7c1731fe611327fb
qaacademy <qaacademy@gmail.com>
2022-05-14 18:03:02 -0300

    TEstes de WINDOW

---
+++ src/test/java/ExerciciosWindow.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExerciciosWindow {
+WebDriver driver;
+    @Before
+    public void before(){
+        driver  = new ChromeDriver();
+        driver.get("file:///Users/mac/Documents/windows.html");
+
+
+    }
+
+    @Test
+    public void testeJanelaSelenium(){
+
+        driver.findElement(By.xpath("//a[@href='http://www.selenium.dev/']//button[@class='btn btn-info'][contains(text(),'click')]")).click();
+        Object[] janelas = driver.getWindowHandles().toArray();
+        driver.switchTo().window(janelas[1].toString());
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://www.selenium.dev/"));
+    }
+
+    @After
+    public void after() throws InterruptedException {
+
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+}
