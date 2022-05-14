680172798fa29de0791bd5f212f7da907062ca0b
qaacademy <qaacademy@gmail.com>
2022-05-14 14:27:04 -0300

    Exercicio 02

--- src/test/java/ExerciciosSelenium.java
+++ src/test/java/ExerciciosSelenium.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +12,8 @@ public class ExerciciosSelenium {
 
     @Test
     public void testeExercicosSelenium() {
+
+        //Exercicio 01
         driver.get("file:///Users/mac/Documents/automation.html");
         driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]"))
                 .sendKeys("Flavio");
@@ -18,5 +22,15 @@ public class ExerciciosSelenium {
         driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("AV Paulista, 509");
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("teste@teste.com.br");
         driver.findElement(By.xpath(" //input[@type='tel']")).sendKeys("119773512");
+        //Exercicio 02
+        driver.findElement(By.xpath("//*[@value='Male']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@value='Male']")).isSelected());
+
+    }
+
+    @After
+    public void after() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
